b5aca47eec4378705c50b3571dffc8255f670503
Bhezad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-04-18 18:05:47 +0430

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter Learning'),
-            centerTitle: true,
-            backgroundColor: Colors.red[500],
-          ),
-          body: Center(
-            child: Text(
-              'hello guys',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey[600],
-                fontFamily: 'SquarePeg'
-              ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: const Text(
-              'click',
-            ),
-            backgroundColor: Colors.red[500],
+      const MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Learning'),
+        centerTitle: true,
+        backgroundColor: Colors.red[500],
+      ),
+      body: Center(
+        child: Text(
+          'hello guys',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'SquarePeg'
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text(
+          'click',
+        ),
+        backgroundColor: Colors.red[500],
+      ),
     );
+  }
+}
+
