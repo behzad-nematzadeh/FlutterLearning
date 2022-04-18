8d84b65a3b6a1eb1c43edd335405848df755db48
Bhezad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-04-18 17:50:42 +0430

    color & text style

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,26 @@ void main() => runApp(
         home: Scaffold(
           appBar: AppBar(
             title: const Text('Flutter Learning'),
+            centerTitle: true,
+            backgroundColor: Colors.red[500],
           ),
-          body: const Center(child: Text('hello guys')),
+          body: Center(
+            child: Text(
+              'hello guys',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {},
-            child: const Text('click'),
+            child: const Text(
+              'click',
+            ),
+            backgroundColor: Colors.red[500],
           ),
         ),
       ),
