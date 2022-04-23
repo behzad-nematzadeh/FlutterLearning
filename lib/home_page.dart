ce4b2ecdd11e395ac4b64c47a4813d445b6e5332
Behzad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-04-24 00:59:13 +0430

    Routing

---
+++ lib/home_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'coworker.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Coworker> coworkers = [
+    Coworker('Mohammad', 'Soltani'),
+    Coworker('Pejman', 'Sharifi'),
+    Coworker('Azam', 'norouzi'),
+    Coworker('Mohsen', 'Pejam'),
+    Coworker('Behzad', 'Nematzadeh'),
+    Coworker('HamidReza', 'Mohammadi'),
+    Coworker('Zahra', 'Nematzadeh'),
+    Coworker('Soudeh', 'Hojatpanah'),
+    Coworker('Soudeh', 'Hashemi'),
+    Coworker('Narges', 'Frahani'),
+    Coworker('Mohammad', 'Babaei'),
+    Coworker('Saeed', 'Dehghani'),
+  ];
+
+  Widget coworkerTemplate(coworker) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              coworker.name,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              coworker.family,
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Coworker List'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.only(bottom: 80),
+        children:
+            coworkers.map((coworker) => coworkerTemplate(coworker)).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_learning/app_theme_data.dart';
-
-import 'coworker.dart';
+import 'package:flutter_learning/home_page.dart';
+import 'package:flutter_learning/login_page.dart';
 
 void main() => runApp(
-      Builder(builder: (context) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: AppThemeData.lightThemeData(context),
-          darkTheme: AppThemeData.darkThemeData(context),
-          home: const CoworkerList(),
-        );
-      }),
-    );
-
-class CoworkerList extends StatefulWidget {
-  const CoworkerList({Key? key}) : super(key: key);
-
-  @override
-  State<CoworkerList> createState() => _CoworkerListState();
-}
-
-class _CoworkerListState extends State<CoworkerList> {
-  List<Coworker> coworkers = [
-    Coworker('Mohammad', 'Soltani'),
-    Coworker('Pejman', 'Sharifi'),
-    Coworker('Azam', 'norouzi'),
-    Coworker('Mohsen', 'Pejam'),
-    Coworker('Behzad', 'Nematzadeh'),
-    Coworker('HamidReza', 'Mohammadi'),
-    Coworker('Zahra', 'Nematzadeh'),
-    Coworker('Soudeh', 'Hojatpanah'),
-    Coworker('Soudeh', 'Hashemi'),
-    Coworker('Narges', 'Frahani'),
-    Coworker('Mohammad', 'Babaei'),
-    Coworker('Saeed', 'Dehghani'),
-  ];
-
-  Widget coworkerTemplate(coworker) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              coworker.name,
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              coworker.family,
-              style: Theme.of(context).textTheme.caption,
-            ),
-          ],
-        ),
+      Builder(
+        builder: (context) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: AppThemeData.lightThemeData(context),
+            darkTheme: AppThemeData.darkThemeData(context),
+            initialRoute: '/',
+            routes: {
+              '/': (context) => const LoginPage(),
+              '/home': (context) => const HomePage(),
+            },
+          );
+        },
       ),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Coworker List'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.only(bottom: 80),
-        children:
-            coworkers.map((coworker) => coworkerTemplate(coworker)).toList(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
