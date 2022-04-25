29887196479e27a4ed82c6f9f392a39b1276d458
Behzad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-04-25 23:08:03 +0430

    BottomAppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter Learning'),
+          body: const Center(child: Text('BottomAppBar')),
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.blueAccent,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
